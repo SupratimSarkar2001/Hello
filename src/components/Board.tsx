a7d79598bcdb65e446850aa4afdaa01f62a2aba5
Supratim <supratimsarkar2001@gmail.com>
2024-03-20 15:41:43 +0530

    Implement: Drag and Drop Cards

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,31 +1,39 @@
 "use client"
+import { useState } from "react";
 import Column from "./Column";
 import NewColumnForm from "./forms/NewColumnForm";
 
-const columns = [
-   { id: "wghgs", name: "todo", index: 0 },
-   { id: "hjshj", name: "doing", index: 1 },
-   { id: "nhsdn", name: "done", index: 2 },
+const defaultColumns = [
+   { id: "col1", name: "todo", index: 0 },
+   { id: "col2", name: "doing", index: 1 },
+   { id: "col3", name: "done", index: 2 },
 ]
 
 export type CardType = {
-   name: String;
-   id: String;
-   order: Number;
+   name: string;
+   id: string | number;
+   index: number;
+   columnId: string
 }
 
-const cards = [
-   { id: "1ghj", name: "card 1", order: 0 , columnId: "wghgs"},
-   { id: "2ghj", name: "card 2", order: 1 , columnId: "hjshj"},
-   { id: "3ghj", name: "card 3", order: 2 , columnId: "nhsdn"},
+const defaultCards = [
+   { id: "1ghj", name: "card 1", index: 0 , columnId: "col1"},
+   { id: "4dhj", name: "card 4", index: 3 , columnId: "col1"},
+   { id: "2ghj", name: "card 2", index: 1 , columnId: "col2"},
+   { id: "3ghj", name: "card 3", index: 2 , columnId: "col3"},
 ]
 export default function Board() {
+   const [cards, setCards] = useState(defaultCards)
+   const [columns, setColumns] = useState(defaultColumns)
    return (
       <div className="flex gap-4">
          {
             columns.map(column => (
               <Column {... column} 
-              cards = {cards.filter(card => card.columnId === column.id)} 
+              setCards = {setCards}
+              cards = {cards.
+               sort((a,b)=>a.index-b.index).
+               filter(card => card.columnId === column.id)} 
               key={column.id} />
             ))
          }
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,18 +1,44 @@
+import { ReactSortable } from "react-sortablejs"
 import { CardType } from "./Board"
+import { SetStateAction } from "react"
 
 type ColumnProps = {
- name: String
+ id: string
+ name: string
  cards: CardType[]
+ setCards: SetStateAction<any>;
 }
-export default function Column({name, cards}: ColumnProps) {
+export default function Column({id,name, cards,setCards}: ColumnProps) {
+
+ const setCardsForColumn=(sortedCards: CardType[], newColumnId: string)=>{
+  setCards((prevCards: CardType[])=>{
+    const newCards= [... prevCards];
+    sortedCards.forEach((sortedCard:CardType, newIndex: number) => {
+      const foundCard = newCards.find(newCard => newCard.id === sortedCard.id);
+      if(foundCard){
+        foundCard.index = newIndex;
+        foundCard.columnId = newColumnId;
+      }
+    })
+    return newCards;
+  })
+ }
+ 
  return(
   <div className="w-48 bg-white shadow-sm rounded-md p-2">
    <h3>{name}</h3>
+   <ReactSortable list={cards} 
+   setList={cards => setCardsForColumn(cards,id)} 
+   group="cards"
+   className="h-full"
+   ghostClass="opacity-40"
+   >
    {cards.map(card => (
-    <div className="border my-2 p-4 rounded-md">
+    <div key={card.id} className="border bg-white my-2 p-4 rounded-md">
      <span>{card.name}</span>
     </div>
    ))}
+   </ReactSortable>
 </div>
  )
 }
