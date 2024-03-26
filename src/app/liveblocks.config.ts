bc9ba89aa7adf6b14fc7c4fa482a3ee5297267cd
Supratim <supratimsarkar2001@gmail.com>
2024-03-26 11:14:32 +0530

    INIT : liveblocks - client and React

---
+++ src/app/liveblocks.config.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@liveblocks/client";
+import {LiveList, LiveObject} from "@liveblocks/core";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  authEndpoint: "/api/liveblocks-auth",
+  throttle: 100,
+});
